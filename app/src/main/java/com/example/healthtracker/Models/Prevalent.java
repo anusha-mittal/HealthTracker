40b164a045b06e15f0d8ddbc25779fae3f76865a
tushti0110 <56289591+tushti0110@users.noreply.github.com>
2021-02-23 19:03:58 +0530

    adding models

---
+++ app/src/main/java/com/example/healthtracker/Models/Prevalent.java
@@ -0,0 +1,7 @@
+package com.example.healthtracker.Models;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+    public static final String UserPhoneKey="UserPhone";
+    public static final String UserPasswordKey="UserPassword";
+}
